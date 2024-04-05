519d701d100d7f3894c17e2a10ff08f8c372495e
Felipe Lemos <>
2024-04-05 14:25:50 +0200

    Initial Version

---
+++ custom-view-config.mjs
@@ -0,0 +1,27 @@
+/**
+ * @type {import('@commercetools-frontend/application-config').ConfigOptionsForCustomView}
+ */
+const config = {
+  name: 'Felipe Sandbox Custom View',
+  cloudIdentifier: 'gcp-eu',
+  env: {
+    development: {
+      initialProjectKey: 'felipe-sandbox',
+    },
+    production: {
+      customViewId: 'clumn2ho2000izzfa5ablsudt',
+      url: 'https://my-custom-view.com',
+    },
+  },
+  oAuthScopes: {
+    view: ['view_products'],
+    manage: ['manage_products'],
+  },
+  type: 'CustomPanel',
+  typeSettings: {
+    size: 'LARGE',
+  },
+  locators: ['products.product_details.general'],
+};
+
+export default config;
